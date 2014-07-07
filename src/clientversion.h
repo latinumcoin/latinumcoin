d20a27187f4741f4b262d89560d357205ba13043
Bradley <einewton@gmail.com>
2014-07-07 12:56:04 -0600

    Release Version

--- src/clientversion.h
+++ src/clientversion.h
@@ -11,13 +11,13 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
-#define CLIENT_VERSION_REVISION    99
+#define CLIENT_VERSION_REVISION    1
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE  false
+#define CLIENT_VERSION_IS_RELEASE  true
 
-// Copyright year (2009-this)
+// Copyright year (2014-this)
 // Todo: update this when changing our copyright comments in the source
 #define COPYRIGHT_YEAR 2014
 
